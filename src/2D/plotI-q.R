b2b5074e202b2a7795c5db712c0ad406e08b905e
phcerdan <pablo.hernandez.cerdan@outlook.com>
2015-04-15 16:18:10 +1200

    plot with tikzdevice and set option for using lualatex. Change in .vimrc to use lualatex instead of pdftex in latex-box.
    Former-commit-id: 8545354c5091002a3776d6ae7854544e91010176

--- src/2D/plotI-q.R
+++ src/2D/plotI-q.R
@@ -36,34 +36,45 @@ I = data[,"I"];
 # plot(q_trim, I_trim, log="xy");
 
 
-plot(q, I, log="xy");
-lines(q, I)
-title(fname)
-axis(1, q, outer=TRUE, xpd=TRUE)
+# plot(q, I, log="xy");
+# lines(q, I)
+# title(fname)
+# axis(1, q, outer=TRUE, xpd=TRUE)
 
 library("ggplot2")
 library("scales")
 library(tools)
 # pdfName = paste(file_path_sans_ext(filename), ".pdf", sep='');
 # pdf(file=pdfName)
+#####tikzDevice
 library("tikzDevice")
+latexroot="~/devtoolset/texlive/2014/bin/x86_64-linux/"
+options(tikzDefaultEngine = 'luatex')
+# options(tikzDefaultEngine = 'pdftex')
+options(tikzLatex=paste(latexroot,"pdflatex", sep=''));
+options(tikzLualatex=paste(latexroot,"lualatex",sep=''));
+options(tikzXelatex=paste(latexroot,"xelatex", sep=''));
+###############
 tikzNameO = file_path_sans_ext(filename);
 tikzNameO = unlist(strsplit(tikzNameO, "_"));
 tikzName = paste(paste(head(tikzNameO ), collapse=""), ".tex", sep='');
 print(tikzName)
-tikz(file=tikzName, standAlone = TRUE)
-if(q[1] == 0) I[1]=I[2]*10 # for correct I plot limits
+# if(q[1] == 0) I[1]=I[2]*10 # for correct I plot limits
 datf = data.frame(q,I);
 #ADD SAXS DATA
 dataS = read.table("/home/phc/Dropbox/Shared-Geelong-Palmerston/pectin/Pectin1_acid/SAXS_1car200NaCl10A_1237_longMod.txt", col.names=c("d", "I"), row.names=NULL, skip=1);
 qS = dataS[,"d"];
-IS = dataS[,"I"];
+IS = dataS[,"I"]*10^11;
 datafS = data.frame(qS,IS);
-p <-ggplot() +  #data = datf, aes(x = q, y = I)) + theme_bw() + # white background
-    theme(panel.grid.minor = element_blank()) + # remove minor ticks
-    geom_line(data = datf, aes(x = q, y = I)) +
+tikz(file=tikzName, standAlone = TRUE)
+#data = datf, aes(x = q, y = I)) + theme_bw() + # white background
+p <-ggplot()+
+    theme_bw() +
+    # remove minor ticks
+    theme(panel.grid.minor = element_blank()) + 
+    geom_line(data = datf, aes(x=q, y=I)) +
     geom_line(data = datafS, colour="blue", aes(x = qS, y = IS)) + 
-    labs(title = tikzName, x=expression("q [" ~ nm^{-1} ~"]"), y=expression("I")) +
+    # labs(title=expression(tikzName), x=expression("q " ~ nm^{-1} ~""), y=expression("I"))
     scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x, n=4),
                      labels = trans_format("log10", math_format(10^.x))
                      ) +
@@ -75,15 +86,13 @@ p <-ggplot() +  #data = datf, aes(x = q, y = I)) + theme_bw() + # white backgrou
     # slope = log(yend/y)/log(xend/x)
     # geom_segment( aes(x=10^-2, y=10^7, xend=10^-0, yend=10^5)) +  # slope=-1
     # geom_text(aes(x=10^-1.4, y=10^4.5 ), label='a = -1', angle = atan2(-1,1) * 180/pi )  +
-    geom_segment( aes(x=10^-2, y=10^8, xend=10^-1, yend=10^6))  # slope=-2
+    geom_segment( aes(x=10^-2, y=10^14, xend=10^-1, yend=10^12))  # slope=-2
     # geom_text(aes(x=10^-0.4, y=10^4 ), label='a = -2', angle = atan2(-2,1) * 180/pi ) +
     # geom_segment( aes(x=10^-1, y=10^6, xend=10^0, yend=10^3))  # slope=-3
     # geom_text(aes(x=10^-0.4, y=10^5 ), label='a = -3', angle = atan2(-3,1) * 180/pi )
 
 
 #Add new data from SAXS.
-
-
 p
 dev.off(); # To switch off plot.
 # print(paste("Output pdf file generated:", pdfName));
